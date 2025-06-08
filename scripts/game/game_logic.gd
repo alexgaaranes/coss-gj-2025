9601524f695c88a18a6a5e2477d3348c76538070
jcmagpantay <jcmagpantay@up.edu.ph>
2025-06-08 21:57:36 +0800

    fix: Unpaused tree after Reset Button

--- scripts/game/game_logic.gd
+++ scripts/game/game_logic.gd
@@ -212,6 +212,7 @@ func _on_game_lost_triggered() -> void:
 		#await get_tree().create_timer(1.0).timeout
 
 func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 	GlobalSounds.on_restart_pressed()
 
