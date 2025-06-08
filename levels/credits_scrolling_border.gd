7829b05a3821a688daed83942805c4ac34dfb55c
alexgaaranes <aaaranes@up.edu.ph>
2025-06-09 01:28:20 +0800

    feat/credits

--- levels/credits_scrolling_border.gd
+++ levels/credits_scrolling_border.gd
@@ -1,10 +1,9 @@
 extends ParallaxBackground
 
-@export var scrolling_speed = 100
+@export var scrolling_speed = 20
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
