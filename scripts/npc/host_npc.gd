2d2b85e9cf8ee8bd76a1e41196477eb870a492b8
loopsidoop <lawmacj16@gmail.com>
2025-06-08 23:59:28 +0800

    fix: npc map edges

--- scripts/npc/host_npc.gd
+++ scripts/npc/host_npc.gd
@@ -63,7 +63,21 @@ func _process(delta):
 			velocity.x = 0
 		State.WALK:
 			sprite.play("walk")
-			velocity.x = direction * SPEED
+			
+			
+			var hypo_po = position.x + (direction * SPEED)
+			print("hypo pos: ", hypo_po)
+			if hypo_po > 2700:
+				velocity.x = 0
+				pick_random_behavior()
+				return
+			else:
+				if hypo_po < 0:
+					velocity.x = 0
+					pick_random_behavior()
+					return
+				else:
+					velocity.x = direction * SPEED
 		State.TURN:
 			sprite.play("idle")
 			velocity.x = 0
